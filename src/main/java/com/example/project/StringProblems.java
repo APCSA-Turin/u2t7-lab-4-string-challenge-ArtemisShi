6a83d9ee4cc216aff0a2ca0e7fcf9e75e1213317
ArtemisShi <Artemiss2@nycstudents.net>
2024-10-11 12:52:45 +0000

    done

--- src/main/java/com/example/project/StringProblems.java
+++ src/main/java/com/example/project/StringProblems.java
@@ -10,64 +10,129 @@ public class StringProblems{
     // endsLy("oddy") → false
     public boolean endsLy(String x){
             //implement code here
-           return "";
+            if(x.length()<2){
+                return false;
+            }
+            if(x.length() ==2){
+            if(x.equals("ly")){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+            if(x.substring(x.length()-2).equals("ly")){
+                return true;
+            }
+        return false;
     }
 
 
-    // Given two strings, append them together (known as "concatenation") 
-    // and return the result. However, if the concatenation creates a double-char, 
+
+
+    // Given two strings, append them together (known as "concatenation")
+    // and return the result. However, if the concatenation creates a double-char,
     // then omit one of the chars, so "abc" and "cat" yields "abcat".
     // conCat("abc", "cat") → "abcat"
     // conCat("dog", "cat") → "dogcat"
     // conCat("abc", "") → "abc"
     public String conCat(String s1, String s2){
         //implement code here
-        return "";
+        if (s2 == ""){
+            return s1;
+        }
+         if (s1.substring(s1.length()-1).equals(s2.substring(0,1))){
+            return s1 + s2.substring(1);
+        }
+        return s1 + s2;
     }
 
-    // Given a string, return a version without the first 2 chars. 
-    // Except keep the first char if it is 'a' and keep the second char if it is 'b'. 
+
+    // Given a string, return a version without the first 2 chars.
+    // Except keep the first char if it is 'a' and keep the second char if it is 'b'.
     // The string may be any length. Harder than it looks.
     // deFront("Hello") → "llo"
     // deFront("java") → "va"
     // deFront("away") → "aay"
     public String deFont(String s1){
         //implement code here
-        return "";
+       if(s1.substring(0,1).equals("a")){
+    if(s1.substring(1,2).equals("b")){
+        return s1;
+    }
+    return s1.substring(0,1) + s1.substring(2);
+}
+if(s1.substring(1,2).equals("b")){
+    return s1.substring(1);
+}
+        return s1.substring(2);
     }
 
-    
-    // Given a string, if the first or last chars are 'x', 
-    // return the string without those 'x' chars, and otherwise 
+   
+    // Given a string, if the first or last chars are 'x',
+    // return the string without those 'x' chars, and otherwise
     // return the string unchanged.
     // withoutX("xHix") → "Hi"
     // withoutX("xHi") → "Hi"
     // withoutX("Hxix") → "Hxi"
     public String withoutX(String s1){
-        return "";
+        if(s1.substring(0,1).equals("x")){
+            if(s1.substring(s1.length()-1).equals("x")){
+                return s1.substring(1,s1.length()-1);
+            }
+            return s1.substring(1);
+        }
+        if(s1.substring(s1.length()-1).equals("x")){
+return s1.substring(0,s1.length()-1);
+        }
+        return s1;
     }
 
+
+    
+
+
     // Given a string str, if the string starts with "f" return "Fizz".
-    //  If the string ends with "b" return "Buzz". If both the "f" and "b" 
-    //  conditions are true, return "FizzBuzz". In all other cases, return the 
+    //  If the string ends with "b" return "Buzz". If both the "f" and "b"
+    //  conditions are true, return "FizzBuzz". In all other cases, return the
     //  string unchanged. (See also: FizzBuzz Code)
     // fizzString("fig") → "Fizz"
     // fizzString("dib") → "Buzz"
     // fizzString("fib") → "FizzBuzz"
     public String fizzString(String s1){
-        return "";
+         if(s1.substring(0,1).equals("f")){
+            if(s1.substring(s1.length()-1).equals("b")){
+                return "FizzBuzz";
+            }
+            return "Fizz";
+        }
+        if(s1.substring(s1.length()-1).equals("b")){
+        return "Buzz";
+        }
+        return s1;
     }
 
-    // Given an int n, return the string form of the number followed 
-    // by "!". So the int 6 yields "6!". Except if the number is 
-    // divisible by 3 use "Fizz" instead of the number, and if the 
+
+    // Given an int n, return the string form of the number followed
+    // by "!". So the int 6 yields "6!". Except if the number is
+    // divisible by 3 use "Fizz" instead of the number, and if the
     // number is divisible by 5 use "Buzz", and if divisible by both 3 and 5, use "FizzBuzz".
     //  Note: the % "mod" operator computes the remainder after division, so 23 % 10 yields 3.
-    //   What will the remainder be when one number divides evenly into another? 
+    //   What will the remainder be when one number divides evenly into another?
     // fizzString2(1) → "1!"
     // fizzString2(2) → "2!"
     // fizzString2(3) → "Fizz!"
     public String fizzString2(int x){
-        return "";
+     String point = "!";
+        if(x%3 ==0){
+            if(x%5==0){
+                return "FizzBuzz" + point;
+            }
+            return "Fizz" + point;
+        }
+        if(x%5==0){
+            return "Buzz" + point;
+        }
+        return x + point;
     }
 }
---
+++ src/main/java/com/example/project/test.java
@@ -0,0 +1,9 @@
+package com.example.project;
+public class test {
+public static void main(String[] args) {
+    StringProblems cop = new StringProblems();
+System.out.println(cop.conCat("abc",""));
+
+}
+    
+}
